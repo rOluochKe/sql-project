8f923c23b904cec41ba716360b76d259285ad336
Raymond Oluoch <rasholuoch@gmail.com>
2019-09-27 12:02:15 +0300

    adding more queries

--- 07-more-joins.sql
+++ 07-more-joins.sql
@@ -1,77 +1,99 @@
---1. Modify it to show the matchid and player name for all goals scored by Germany. 
---   To identify German players, check for: teamid = 'GER'
-SELECT matchid, player
-FROM goal
-WHERE teamid = 'GER'
+--List the films where the yr is 1962 [Show id, title]
+SELECT id, title
+ FROM movie
+ WHERE yr=1962
 
---2. Show id, stadium, team1, team2 for just game 1012
-SELECT id, stadium, team1, team2
-FROM game
-WHERE id = '1012'
+--Give year of 'Citizen Kane'
+ SELECT yr
+FROM movie
+WHERE title = 'Citizen Kane'
+--List all of the Star Trek movies, include the id, title and yr (all of these movies include the words Star Trek in the title). Order results by year.
+SELECT id, title, yr
+FROM  movie
+WHERE title LIKE '%Star Trek%'
+ORDER BY yr
 
---3. Modify it to show the player, teamid, stadium and mdate for every German goal.
-SELECT player, teamid, stadium, mdate
-FROM game JOIN goal ON (game.id = goal.matchid )
-WHERE goal.teamid = 'GER'
+--What id number does the actor 'Glenn Close' have?
+SELECT id
+FROM actor
+WHERE name = 'Glenn Close'
 
---4. Show the team1, team2 and player for every goal scored by a player called Mario player LIKE 'Mario%'
-SELECT team1, team2, player
-FROM game JOIN goal ON (game.id = goal.matchid)
-WHERE player LIKE 'Mario%'
+--What is the id of the film 'Casablanca'
+SELECT id
+FROM  movie
+WHERE title = 'Casablanca'
 
---5. Show player, teamid, coach, gtime for all goals scored in the first 10 minutes gtime<=10
-SELECT player, teamid, coach, gtime
-FROM goal JOIN eteam ON (teamid=id)
-WHERE gtime<=10
+--Obtain the cast list for 'Casablanca'.
+SELECT actor.name
+FROM  movie
+JOIN casting ON movie.id = casting.movieid
+JOIN actor ON actor.id = casting.actorid
+WHERE title = 'Casablanca'
 
---6. List the the dates of the matches and the name of the team in which 'Fernando Santos' was the team1 coach.
-SELECT game.mdate, eteam.teamname
-FROM game JOIN eteam ON (game.team1 = eteam.id)
-WHERE eteam. coach = 'Fernando Santos'
+--SELECT actor.name
+FROM actor
+JOIN casting ON actor.id = casting.actorid
+JOIN movie ON movie.id = casting.movieid
+WHERE movie.title = 'Alien'
 
---7. List the player for every goal scored in a game where the stadium was 'National Stadium, Warsaw'
-SELECT player
-FROM goal
-  JOIN game ON (matchid = id)
-WHERE stadium =  'National Stadium, Warsaw'
+--List the films in which 'Harrison Ford' has appeared
+SELECT movie.title
+FROM movie
+JOIN casting ON casting.movieid = movie.id
+JOIN actor ON actor.id = casting.actorid
+WHERE actor.name = 'Harrison Ford'
 
---8. Show the name of all players who scored a goal against Germany.
-SELECT DISTINCT(player)
-FROM game JOIN goal ON matchid = id
-WHERE (teamid!='GER') AND team1 = 'GER' OR (teamid!='GER') AND team2 = 'GER'
+--List the films where 'Harrison Ford' has appeared - but not in the starring role. [Note: the ord field of casting gives the position of the actor. If ord=1 then this actor is in the starring role
+SELECT movie.title
+FROM movie
+JOIN casting ON movie.id = casting.movieid
+JOIN actor ON actor.id = casting.actorid
+WHERE casting.ord != 1 AND actor.name = 'Harrison Ford'
 
---9. Show teamname and the total number of goals scored.
-SELECT teamname, COUNT (*)
-FROM eteam JOIN goal ON id=teamid
-GROUP BY teamname
+--List the films together with the leading star for all 1962 films
+SELECT movie.title, actor.name
+FROM movie
+JOIN casting ON movie.id = casting.movieid
+JOIN actor ON actor.id = casting.actorid
+WHERE casting.ord = 1 AND movie.yr = 1962
 
---10. Show the stadium and the number of goals scored in each stadium.
-SELECT stadium, COUNT (*)
-FROM game JOIN goal ON id=matchid
-GROUP BY stadium
+--Which were the busiest years for 'Rock Hudson', show the year and the number of movies he made each year for any year in which he made more than 2 movies.
+SELECT yr,COUNT(title) FROM
+  movie JOIN casting ON movie.id=movieid
+        JOIN actor   ON actorid=actor.id
+WHERE name='Rock Hudson'
+GROUP BY yr
+HAVING COUNT(title) > 2
 
---11. For every match involving 'POL', show the matchid, date and the number of goals scored.
-SELECT game.id, game.mdate, COUNT(*)
-FROM game
-  JOIN goal
-  ON game.id = goal.matchid
-WHERE (game.team1 = 'POL' OR game.team2 = 'POL')
-GROUP BY game.id, game.mdate
+--List the film title and the leading actor for all of the films 'Julie Andrews' played in.
+SELECT movie.title, actor.name
+FROM movie
+JOIN casting ON casting.movieid = movie.id
+JOIN actor ON casting.actorid = actor.id
+WHERE casting.ord = 1 AND movie.id IN (
+SELECT movieid FROM casting WHERE actorid = 179)
 
---12. For every match where 'GER' scored, show matchid, match date and the number of goals scored by 'GER'
-SELECT id, mdate, COUNT(*)
-FROM game
-  JOIN goal
-  ON goal.matchid = game.id
-WHERE goal.teamid = 'GER'
-GROUP BY id, mdate
+--Obtain a list, in alphabetical order, of actors who've had at least 30 starring roles
+SELECT distinct name
+FROM actor
+  JOIN casting
+  ON actor.id=casting.actorid and ord=1
+GROUP BY name
+HAVING COUNT(ord)>=30
+ORDER BY name
 
---13. List every match with the goals scored by each team as shown. This will use "CASE WHEN" 
---    which has not been explained in any previous exercises.
-SELECT mdate, team1,
-  SUM(CASE WHEN teamid=team1 THEN 1 ELSE 0 END) score1,
-  team2,
-  SUM(CASE WHEN teamid=team2 THEN 1 ELSE 0 END) score2
-FROM game LEFT JOIN goal ON matchid = id
-GROUP BY id, mdate, team1, team2
-ORDER BY mdate, matchid, team1, team2
+--List the films released in the year 1978 ordered by the number of actors in the cast, then by title
+SELECT movie.title, COUNT(actorid)
+FROM movie
+JOIN casting ON casting.movieid = movie.id
+JOIN actor ON casting.actorid = actor.id
+WHERE movie.yr = 1978
+GROUP BY movie.title
+ORDER BY COUNT(actorid) DESC, movie.title
+
+-- List all the people who have worked with 'Art Garfunkel'.
+SELECT actor.name
+FROM actor
+  JOIN casting
+  ON actor.id=casting.actorid
+WHERE movieid IN (10095, 11434, 13630) AND actor.name != 'Art Garfunkel'
